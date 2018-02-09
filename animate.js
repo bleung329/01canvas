4075b718a95c934292e0097dc9915a8fbdb9ef37
bleung329 <bleung@stuy.edu>
2018-02-09 05:22:38 -0500

    woohoo

--- animate.js
+++ animate.js
@@ -3,6 +3,7 @@ Brian Leung
 SoftDev PD 7 
 MyFirstAnimation
 */
+
 var canvas = document.getElementById('slate')
 var ctx = canvas.getContext('2d')
 var animation
@@ -16,17 +17,41 @@ var growthSpeed = 10
 var dvdX = 10
 var dvdY = 10
 
-var vector
+//Vector and the helper functions
+var vector = {x:2,y:3}
+
+function reflectVert()
 {
-	
+	vector.y = -1 * vector.y
+}
+function reflectSide()
+{
+	vector.x = -1 * vector.x
 }
 
 function dvdPlayer()
 {
 	ctx.clearRect(0,0, canvas.width, canvas.height)
+	drawCircle(dvdX,dvdY,20)
+	dvdX += vector.x
+	dvdY += vector.y
+	/*
+	console.log("dvdX: "+dvdX)
+	console.log("dvdY: "+dvdY)
+	console.log(vector.x)
+	*/
+	if (dvdX>=600 || dvdX<=0)
+	{
+		reflectSide()
+	}
+	if (dvdY>=600 || dvdY<=0)
+	{
+		reflectVert()
+	}
 	animation = window.requestAnimationFrame(dvdPlayer)
 }
 
+//Growing and shrinking circle function
 function growCircle()
 {
 	ctx.clearRect(0,0, canvas.width, canvas.height)
@@ -47,30 +72,33 @@ function growCircle()
 		shrinking = 0
 	}
 	drawCircle(300,300,circleSize)
-	animation = window.requestAnimationFrame(draw)
+	animation = window.requestAnimationFrame(growCircle)
 }
 
+
+//For drawing circle, used in both animations
 function drawCircle(x,y,radius)
 {
 	ctx.beginPath();
-    ctx.arc(300,300, radius, 0, Math.PI * 2);
+    ctx.arc(x,y, radius, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fillStyle = 'blue';
     ctx.fill();
 }
 
+
+//Attaching functions to buttons
 document.getElementById('toggle').onclick = function()
 {
-	draw()
+	growCircle()
+}
+document.getElementById('dvdplayer').onclick = function()
+{
+	dvdPlayer()
 }
 document.getElementById('pause').onclick = function()
 {
 	window.cancelAnimationFrame(animation)
 }
-document.getElementById('dvdplayer').onclick = function()
-{
-	window.cancelAnimationFrame(animation)
-}
 
-
-draw()
+growCircle()
